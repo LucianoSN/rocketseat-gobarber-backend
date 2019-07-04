bd67c91638749cf2aab8e1c6ae8f1a650c89e4ff
Luciano de Souza Nery <lsnery@gmail.com>
2019-07-04 17:36:28 -0300

    adicionado upload de avatar

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true
-    },
-    extends: ['airbnb-base', 'prettier'],
-    plugins: ['prettier'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module'
-    },
-    rules: {
-        'prettier/prettier': 'error',
-        'class-methods-use-this': 'off',
-        'no-param-reassign': 'off',
-        "no-console": "off",
-        camelcase: 'off',
-        'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-        "linebreak-style": 0,
-        "no-tabs": 0
-    }
+	env: {
+		es6: true,
+		node: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	plugins: ['prettier'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		'class-methods-use-this': 'off',
+		'no-param-reassign': 'off',
+		'no-console': 'off',
+		camelcase: 'off',
+		'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+		'linebreak-style': 0,
+		'no-tabs': 0,
+	},
 };
